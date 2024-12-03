4dfe170f64c1387efd11d69d43db996b60c0ae98
Shteevee <ewan.corbett@outlook.com>
2024-12-03 03:30:10 +0000

    add linting to elixir code

--- 1/solution.ex
+++ 1/solution.ex
@@ -1,33 +1,33 @@
 defmodule Day1 do
-  def stringListToIntList(list) do
+  def string_list_to_int_list(list) do
     Enum.map(list, &(Integer.parse(&1) |> elem(0)))
   end
 
-  def parseFile(file) do
-    String.split(file, "\n") |> Enum.map(&(String.split(&1)) |> stringListToIntList)
+  def parse_file(file) do
+    String.split(file, "\n") |> Enum.map(&(String.split(&1) |> string_list_to_int_list))
   end
 
-  def pairSmallestValues(list) do
-    first = Enum.map(list, fn [x, _] -> x end) |> Enum.sort
-    second = Enum.map(list, fn [_, x] -> x end) |> Enum.sort
+  def pair_smallest_values(list) do
+    first = Enum.map(list, fn [x, _] -> x end) |> Enum.sort()
+    second = Enum.map(list, fn [_, x] -> x end) |> Enum.sort()
     Enum.zip(first, second)
   end
 
-  def sumPairDistances(list) do
-    Enum.reduce(list, 0, fn {x, y}, acc -> acc + abs(x-y) end)
+  def sum_pair_distances(list) do
+    Enum.reduce(list, 0, fn {x, y}, acc -> acc + abs(x - y) end)
   end
 
-  def createOccurrenceMap(list) do
-    Enum.reduce(list, Map.new(), fn [_, x], acc -> Map.put(acc, x, Map.get(acc, x, 0)+1)  end)
+  def create_occurrence_map(list) do
+    Enum.reduce(list, Map.new(), fn [_, x], acc -> Map.put(acc, x, Map.get(acc, x, 0) + 1) end)
   end
 
-  def calcSimilarityScore(occurrenceMap, list) do
-    Enum.reduce(list, 0, fn [x, _], acc -> acc + (x * Map.get(occurrenceMap, x, 0)) end)
+  def calc_similarity_score(occurrence_map, list) do
+    Enum.reduce(list, 0, fn [x, _], acc -> acc + x * Map.get(occurrence_map, x, 0) end)
   end
 end
 
-input = File.read!("input.txt") |> Day1.parseFile
-summedSmallestPairs = Day1.pairSmallestValues(input) |> Day1.sumPairDistances
-IO.inspect(summedSmallestPairs, label: "Part 1")
-similarityScore = Day1.createOccurrenceMap(input) |> Day1.calcSimilarityScore(input)
-IO.inspect(similarityScore, label: "Part 2")
+input = File.read!("input.txt") |> Day1.parse_file()
+summed_smallest_pairs = Day1.pair_smallest_values(input) |> Day1.sum_pair_distances()
+IO.puts("Part 1: #{summed_smallest_pairs}")
+similarity_score = Day1.create_occurrence_map(input) |> Day1.calc_similarity_score(input)
+IO.puts("Part 2: #{similarity_score}")
--- 2/solution.ex
+++ 2/solution.ex
@@ -1,65 +1,79 @@
 defmodule Day2 do
-  def stringListToIntList(list) do
+  def string_list_to_int_list(list) do
     Enum.map(list, &(Integer.parse(&1) |> elem(0)))
   end
 
-  def parseFile(file) do
-    String.split(file, "\n") |> Enum.map(&(String.split(&1)) |> stringListToIntList)
+  def parse_file(file) do
+    String.split(file, "\n") |> Enum.map(&(String.split(&1) |> string_list_to_int_list))
   end
 
-  def diffInBounds(x, y) do
+  def diff_in_bounds(x, y) do
     diff = x - y
     diff >= 1 && diff <= 3
   end
 
-  def inBounds([x, y | tail], func) do
-    inBounds([y | tail], func, func.(x, y))
+  def in_bounds([x, y | tail], func) do
+    in_bounds([y | tail], func, func.(x, y))
   end
 
-  def inBounds([x, y | tail], func, acc) do
-    newAcc = func.(x, y) && acc
+  def in_bounds([x, y | tail], func, acc) do
+    new_acc = func.(x, y) && acc
+
     if length(tail) > 0 do
-      inBounds([y | tail], func, newAcc)
+      in_bounds([y | tail], func, new_acc)
     else
-      newAcc
+      new_acc
     end
   end
 
-  def reportIsSafe(report) do
-    diffInBoundsDsc = fn x, y -> diffInBounds(x, y) end
-    diffInBoundsAsc = fn x, y -> diffInBounds(y, x) end
-    inBounds(report, diffInBoundsAsc) || inBounds(report, diffInBoundsDsc)
+  def report_is_safe(report) do
+    diff_in_bounds_dsc = fn x, y -> diff_in_bounds(x, y) end
+    diff_in_bounds_asc = fn x, y -> diff_in_bounds(y, x) end
+    in_bounds(report, diff_in_bounds_asc) || in_bounds(report, diff_in_bounds_dsc)
   end
 
-  def countSafeReports(reports) do
+  def count_safe_reports(reports) do
     Enum.reduce(
       reports,
       0,
-      fn report, acc -> if reportIsSafe(report) do acc + 1 else acc end end
+      fn report, acc ->
+        if report_is_safe(report) do
+          acc + 1
+        else
+          acc
+        end
+      end
     )
   end
 
-  def reportIsSafeWithTolerance(report) do
-    reportVariants = [report |
-      Enum.reduce(
-        0..length(report)-1,
-        [],
-        fn idx, acc -> [ List.delete_at(report, idx) | acc ] end
-      )
+  def report_is_safe_with_tolerance(report) do
+    report_variants = [
+      report
+      | Enum.reduce(
+          0..(length(report) - 1),
+          [],
+          fn idx, acc -> [List.delete_at(report, idx) | acc] end
+        )
     ]
 
-    Enum.map(reportVariants, &(reportIsSafe(&1))) |> Enum.any?
+    Enum.map(report_variants, &report_is_safe(&1)) |> Enum.any?()
   end
 
-  def countSafeReportsWithTolerance(reports) do
+  def count_safe_reports_with_tolerance(reports) do
     Enum.reduce(
       reports,
       0,
-      fn report, acc -> if reportIsSafeWithTolerance(report) do acc + 1 else acc end end
+      fn report, acc ->
+        if report_is_safe_with_tolerance(report) do
+          acc + 1
+        else
+          acc
+        end
+      end
     )
   end
 end
 
-reports = File.read!("input.txt") |> Day2.parseFile
-IO.inspect(Day2.countSafeReports(reports), label: "Part 1")
-IO.inspect(Day2.countSafeReportsWithTolerance(reports), label: "Part 2")
+reports = File.read!("input.txt") |> Day2.parse_file()
+IO.puts("Part 1: #{Day2.count_safe_reports(reports)}")
+IO.puts("Part 2: #{Day2.count_safe_reports_with_tolerance(reports)}")
