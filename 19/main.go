ab80bc882e99dbf6e00add6111f76c55a6076de3
Shteevee <ewan.corbett@outlook.com>
2024-12-19 18:05:59 +0000

    day 19 part 2

--- 19/main.go
+++ 19/main.go
@@ -50,6 +50,35 @@ func countPossibleDesigns(patterns map[rune][][]rune, designs [][]rune) int {
 	return possible
 }
 
+func allPossibleTowelCombos(patterns map[rune][][]rune, design []rune, cache map[string]int) int {
+	val, cacheHit := cache[string(design)]
+	switch {
+	case cacheHit:
+		return val
+	case len(design) == 0:
+		return 1
+	default:
+		possible := 0
+		for _, pattern := range patterns[design[0]] {
+			if len(design) >= len(pattern) && slices.Equal(pattern, design[0:len(pattern)]) {
+				p := allPossibleTowelCombos(patterns, design[len(pattern):], cache)
+				cache[string(design[len(pattern):])] = p
+				possible += p
+			}
+		}
+		return possible
+	}
+}
+
+func countAllPossibleTowelCombos(patterns map[rune][][]rune, designs [][]rune) int {
+	cache := map[string]int{}
+	possible := 0
+	for _, design := range designs {
+		possible += allPossibleTowelCombos(patterns, design, cache)
+	}
+	return possible
+}
+
 func main() {
 	start := time.Now()
 	file, err := os.Open("input.txt")
@@ -66,6 +95,7 @@ func main() {
 	patterns, designs := parseTowels(scanner)
 
 	fmt.Println("Part 1:", countPossibleDesigns(patterns, designs))
+	fmt.Println("Part 2:", countAllPossibleTowelCombos(patterns, designs))
 
 	log.Printf("Time taken: %s", time.Since(start))
 }
